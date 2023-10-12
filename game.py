18656adff40c54a3cd66afa9495c4ed70c22f353
Austin Berry <aberry52098@gmail.com>
2023-10-12 15:19:37 -0400

    step 1 make goblin/hero objects

--- game.py
+++ game.py
@@ -1,9 +1,20 @@
+
 class Fighter:
-    def __init__(self, name, power, health):
+    def __init__(self, name = "", type = "", power = 10, health = 100):
       self.name = name
+      self.type = type
       self.power = power
       self.health = health
 
     def attack(self, other_person):
       print(f'{self.name} attacks {other_person.name}!')
-
+    
+    def stats(self):
+      print(f"""Name: {self.name}
+Type: {self.type}
+Power: {self.power}
+Health: {self.health}
+""")
+###########################################
+goblin = Fighter()
+hero = Fighter()
